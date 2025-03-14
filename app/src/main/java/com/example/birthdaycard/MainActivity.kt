beb45433ee74428bea41480fa15dd293b212ad9b
keithunt-35 <keithunt35@duck.com>
2025-03-14 20:36:36 +0300

    complete ui

--- app/src/main/java/com/example/birthdaycard/MainActivity.kt
+++ app/src/main/java/com/example/birthdaycard/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -12,8 +15,11 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.birthdaycard.ui.theme.BirthdayCardTheme
 
@@ -26,7 +32,14 @@ class MainActivity : ComponentActivity() {
                 //  A surface container using the background color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background){
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    GreetingText(
+                        message = "Happy Birthday Osbert!",
+                        from = "from Keith",
+                        modifier = Modifier.padding(8.dp)
+                    )
+
 
                 }
             }
@@ -36,15 +49,22 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
-    Text(
-        text = message,
-        fontSize = 100.sp,
-        lineHeight = 116.sp
-    )
-    Text(
-        text = from,
-        fontSize = 36.sp
-    )
+    Column (
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ){
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 20.sp,
+            modifier = Modifier.padding(16.dp).align(alignment = Alignment.End)
+        )
+    }
 }
 
 
